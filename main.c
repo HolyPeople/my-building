ee89cec549e52c0068c18ee2a304786a5419ed98
HolyPeope <holyjirung@konkuk.ac.kr>
2018-12-05 11:22:17 +0900

    My_building_develop_L2

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define Room Person*
+#define Floor Room*
 
 typedef struct _person {
 	char name[10];
@@ -10,30 +12,34 @@ typedef struct _person {
 }Person;
 
 int show_menu();
-void init_room(Room*); //initialization Room and dynamic alloc Person array
-void asign_room(Room*); //asign Person data to Room
+void init_room(Floor*); //initialization Room and dynamic alloc Person array
+void asign_room(Floor*); //asign Person data to Room
 void delete_room(Room); //retrun Person array
-void print_room(Room); //print room_data
+void print_room(Floor); //print room_memory
+void init_floor(Floor*); // initialization Room and dynamic alloc Person 2D array
+void delete_floor(Floor); //return floor_memory
 
 int main(void) {
 
-	Room my_room = NULL;
+	Floor my_floor = NULL;
 	int menu = 0;
 	
 	while (1) {
 		menu = show_menu();
+
 		if (!menu) break;
 		switch (menu)
 		{
-		case 1: init_room(&my_room); break;
-		case 2: asign_room(&my_room); break;
-		case 3: print_room(my_room); break;
+		case 1: init_room(&my_floor); break;
+		case 2: asign_room(&my_floor); break;
+		case 3: print_room(my_floor); break;
+		case 4: init_floor(&my_floor); break;
 		}
 		system("pause");
 		system("cls");
 	}
 	
-	delete_room(my_room);
+	delete_floor(my_floor);
 
 	return 0;
 }
@@ -42,12 +48,14 @@ int show_menu() {
 
 	char ch;
 
-	printf("0. 종료 1. 방 만들기 2.방에 입주 3. 방 정보 출력\n");
+	printf("0. 종료\n1. 방 만들기 2.방에 입주 3. 방 정보 출력\n");
+	printf("4. 층 만들기\n");
 	printf("입력 : ");
 
-	scanf_s("%c", &ch, sizeof(ch));
+	scanf_s(" %c", &ch, sizeof(ch));
 	while (getchar() != '\n');
-	if ('0' <= ch && ch <= '3') {
+
+	if ('0' <= ch && ch <= '4') {
 		return ch - '0';
 	}
 	
@@ -60,37 +68,53 @@ int show_menu() {
 }
 
 //initialization Room and dynamic alloc Person array
-void init_room(Room* room_p) {
+void init_room(Floor* floor_p) {
 
 	int size;
+	int i;
+	int room = _msize(*floor_p) / sizeof(Room);
 
-	if (*room_p) return;
+	if (!(*floor_p)) return;
 
-	printf("방에 몇명의 거주자가 있나요? : ");
-	scanf_s("%d", &size);
+	for (i = 0; i < room; i++) {
+		printf("10%d호에는 몇명의 거주자가 있나요? : " , i+1);
+		scanf_s("%d", &size);
 
-	if (size <= 0 || size > 2) {
-		printf("잘못된 입력입니다. 다시 입력해 주세요\n");
-		init_room(room_p);
-		return;
+		if (size <= 0 || size > 2) {
+			printf("잘못된 입력입니다. 다시 입력해 주세요\n");
+			i--;
+			continue;
+		}
+		*(*floor_p + i) = (Room)malloc(sizeof(Person) * size);
 	}
-	*room_p = (Room)malloc(sizeof(Person) * size);
+
+	
+	
 }
 
 //asign Person data to Room
-void asign_room(Room* room_p) {
+void asign_room(Floor* floor_p) {
 
-	int i;
+	int i,j;
+	int room, person;
 
-	if (!(*room_p)) return;
+	if (!(*floor_p)) return;
 
-	for (i = 0; i < _msize(*room_p) / sizeof(Person); i++) {
-		printf("%d번 거주자의 이름 : ", i+1);
-		scanf_s("%s", room_p[i]->name, sizeof(room_p[i]->name));
-		printf("%d번 거주자의 나이 : ", i+1);
-		scanf_s("%d", &(room_p[i]->age));
-		printf("%d번 거주자의 전화번호 : ", i+1);
-		scanf_s("%s", room_p[i]->phone_number, sizeof(room_p[i]->phone_number));
+
+	room = _msize(*floor_p) / sizeof(Room);
+	person = _msize((*floor_p)[0]) / sizeof(Person);
+
+	for (i = 0; i < room; i++) {
+		printf("%3d호 정보 입력\n", i + 101);
+		for (j = 0; j < person; j++) {
+			printf("%d번 거주자의 이름 : ", j+ 1);
+			scanf_s("%s", floor_p[i][j]->name, sizeof(floor_p[i][j]->name));
+			printf("%d번 거주자의 나이 : ", j + 1);
+			scanf_s("%d", &(floor_p[i][j]->age));
+			printf("%d번 거주자의 전화번호 : ", j + 1);
+			scanf_s("%s", floor_p[i][j]->phone_number, sizeof(floor_p[i][j]->phone_number));
+		}
+		
 	}
 }
 
@@ -102,15 +126,56 @@ void delete_room(Room room) {
 }
 
 //print room_data
-void print_room(Room room) {
+void print_room(Floor floor) {
+
+	int i,j;
+	int room, person;
+
+	if (!floor) return;
+
+	room = _msize(floor) / sizeof(Room);
+	person = _msize(floor[0]) / sizeof(Person);
+
+	for (i = 0; i < room; i++) {
+		printf("[10%d]호\n", i + 1);
+		for (j = 0; j < person; j++) {
+			printf("%d번 거주자의 이름 : %s\n", i + 1, floor[i][j].name);
+			printf("%d번 거주자의 나이 : %d\n", i + 1, floor[i][j].age);
+			printf("%d번 거주자의 전화번호 : %s\n", i + 1, floor[i][j].phone_number);
+		}
+	}
+}
+
+// initialization Room and dynamic alloc Person 2D array
+void init_floor(Floor* floor_p) {
+	int size;
+
+	if (*floor_p) return;
+	
+	printf("층에 몇개의 방이 있나요 : ");
+	scanf_s("%d", &size);
+
+	if (size <= 0 || size > 4) {
+		printf("잘못된 입력입니다.");
+		init_floor(floor_p);
+		return;
+	}
+
+	*floor_p = (Floor)malloc(sizeof(Room) * size);
+}
+
+//return floor_memory
+void delete_floor(Floor floor) {
 
 	int i;
+	int room;
 
-	if (!room) return;
+	if (!floor) return;
 
-	for (i = 0; i < _msize(room) / sizeof(Person); i++) {
-		printf("%d번 거주자의 이름 : %s\n", i+1,room[i].name);
-		printf("%d번 거주자의 나이 : %d\n", i+1,room[i].age);
-		printf("%d번 거주자의 전화번호 : %s\n", i+1,room[i].phone_number);
+	room = _msize(floor) / sizeof(Room);
+
+	for (i = 0; i < room; i++) {
+		delete_room(floor[i]);
 	}
+	free(floor);
 }
